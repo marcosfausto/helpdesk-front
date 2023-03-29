740d44305d8056b7a97dea2a5ac589a2c0d6094b
Marcos <marcos_fausto@yahoo.com.br>
2023-03-29 21:19:04 +0100

    HTML e CSS para componente chamado-list

--- src/app/components/models/chamado.ts
+++ src/app/components/models/chamado.ts
@@ -5,7 +5,7 @@ export interface Chamado {
     prioridade:      string;
     status:          string;
     titulo:          string;
-    observacoes:     string;
+    descricao:       string;
     tecnico:            any;
     cliente:            any;
     nomeCliente:     string;
